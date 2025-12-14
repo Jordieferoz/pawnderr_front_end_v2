c195f78a662385aa0de88b66c10ac25df472f431
arun.periasamy <arun.raja103@gmail.com>
2025-12-14 19:17:26 +0530

    feat: home page component added

---
+++ ui_components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+
+const Home = () => {
+  return (
+    <div className="relative min-h-screen">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
---
+++ ui_components/Home/index.ts
@@ -0,0 +1,4 @@
+import Hero from "./Hero";
+import Home from "./Home";
+
+export { Hero, Home };
